b23150165a1f529e08fb6ffbd37f985dd80f7d5a
Regis-Caelum <khanmf@rknec.edu>
2021-09-11 12:07:24 +0530

    aws ready

--- vtechssl/settings.py
+++ vtechssl/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1^#z09x5kfx!5q0jd_7)wk!xaz$z#ilv7b=-u0)4-*-r7(yum6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.235.16.132']
 
 
 # Application definition
